c0df104e6f8f9fcc0493a4f85fb6b964934da046
SergTa <88887008+SergTa@users.noreply.github.com>
2024-05-16 15:52:33 +0300

    Update model_preprocessing.py
    Added Preprocessings utils

--- model_preprocessing.py
+++ model_preprocessing.py
@@ -6,15 +6,82 @@ import numpy as np # библиотека Numpy для операций лине
 
 
 # предварительная обработка числовых признаков
-from sklearn.preprocessing import MinMaxScaler# Импортируем нормализацию от scikit-learn
 from sklearn.preprocessing import StandardScaler # Импортируем стандартизацию от scikit-learn
 from sklearn.preprocessing import PowerTransformer  # Степенное преобразование от scikit-learn
-# предварительная обработка категориальных признаков
-from sklearn.preprocessing import OneHotEncoder# Импортируем One-Hot Encoding от scikit-learn
-from sklearn.preprocessing import OrdinalEncoder# Импортируем Порядковое кодированиеот scikit-learn
 
-from sklearn.pipeline import Pipeline # Pipeline. Ни добавить, ни убавить
-
+from sklearn.pipeline import Pipeline # Pipeline
 from sklearn.compose import ColumnTransformer # т.н. преобразователь колонок
-
 from sklearn.base import BaseEstimator, TransformerMixin # для создания собственных преобразователей / трансформеров данных
+
+
+class QuantileReplacer(BaseEstimator, TransformerMixin):
+    def __init__(self, threshold=0.05):
+        self.threshold = threshold
+        self.quantiles = {}
+
+    def fit(self, X, y=None):
+        for col in X.select_dtypes(include='number'):
+            low_quantile = X[col].quantile(self.threshold)
+            high_quantile = X[col].quantile(1 - self.threshold)
+            self.quantiles[col] = (low_quantile, high_quantile)
+        return self
+
+    def transform(self, X):
+        X_copy = X.copy()
+        for col in X.select_dtypes(include='number'):
+            low_quantile, high_quantile = self.quantiles[col]
+            rare_mask = ((X[col] < low_quantile) | (X[col] > high_quantile))
+            if rare_mask.any():
+                rare_values = X_copy.loc[rare_mask, col]
+                replace_value = np.mean([low_quantile, high_quantile])
+                if rare_values.mean() > replace_value:
+                    X_copy.loc[rare_mask, col] = high_quantile
+                else:
+                    X_copy.loc[rare_mask, col] = low_quantile
+        return X_copy
+
+class RareGrouper(BaseEstimator, TransformerMixin):
+    def __init__(self, threshold=0.05, other_value='Other'):
+        self.threshold = threshold
+        self.other_value = other_value
+        self.freq_dict = {}
+
+    def fit(self, X, y=None):
+        for col in X.select_dtypes(include=['object']):
+            freq = X[col].value_counts(normalize=True)
+            self.freq_dict[col] = freq[freq >= self.threshold].index.tolist()
+        return self
+
+    def transform(self, X, y=None):
+        X_copy = X.copy()
+        for col in X.select_dtypes(include=['object']):
+            X_copy[col] = X_copy[col].apply(lambda x: x if x in self.freq_dict[col] else self.other_value)
+        return X_copy
+
+num_pipe_distance = Pipeline([
+    ('QuantReplace', QuantileReplacer(threshold=0.01, )),
+    ('scaler', StandardScaler())
+])
+num_distance = ['Distance']
+
+num_pipe_engine = Pipeline([
+    ('scaler', StandardScaler())
+])
+num_engine = ['Engine_capacity(cm3)']
+
+num_pipe_year = Pipeline([
+    ('power', PowerTransformer())
+])
+num_year = ['Year']
+
+# Pipeline с числовыми признаками
+preprocessors_num = ColumnTransformer(transformers=[
+    ('num_distance', num_pipe_distance, num_distance),
+    ('num_engine', num_pipe_engine, num_engine),
+    ('num_year', num_pipe_year, num_year),
+])
+
+# объединяем названия колонок в один список (важен порядок как в ColumnTransformer)
+columns_num = np.hstack([num_distance,
+                    num_engine,
+                    num_year,])
