59ff37b633f396f5896e7a24cbf3fcfccbf5ef50
SergTa <tabakar456@mail.ru>
2024-05-20 11:14:25 +0300

    Edited bash-script

--- pipeline.sh
+++ pipeline.sh
@@ -2,7 +2,7 @@
 
 n_datasets=$1 # Установка номера дата-сета
 
-python data_creation.py 
-python model_preprocessing.py
-python model_prepfrftion.py
-python model_testing.py
+python data_creation.py $n_datasets
+python model_preprocessing.py $n_datasets
+python model_prepfrftion.py $n_datasets
+python model_testing.py $n_datasets
