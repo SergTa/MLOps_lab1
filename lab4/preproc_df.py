c3c6e67ff23249d8bea7034ba78278210d647232
SergTa <tabakar456@mail.ru>
2024-06-05 17:18:27 +0300

    Edited paths

--- lab4/preproc_df.py
+++ lab4/preproc_df.py
@@ -2,11 +2,11 @@
 import pandas as pd
 
 # Загрузка датасета
-df = pd.read_csv('../datasets/titanic/titanic_df.csv')
+df = pd.read_csv('../datasets/titanic_df.csv')
 
 # Заполнение пропущенных значений в поле "Age" средним значением
 mean_age = df['Age'].mean()
 df['Age'] = df['Age'].fillna(mean_age)
 
 # Сохранение новой версии датасета
-df.to_csv('../datasets/titanic/titanic_df.csv', index=False)
+df.to_csv('../datasets/titanic_df.csv', index=False)
