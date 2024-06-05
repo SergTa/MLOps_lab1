0b0f6c4031a69f567e008e35feaf754c7fdb3352
SergTa <tabakar456@mail.ru>
2024-06-05 17:13:23 +0300

    Edited one-hot-encoding

---
+++ lab4/model.py
@@ -0,0 +1,15 @@
+#Загрузка библиотек
+import pandas as pd
+
+
+# Загрузка датасета
+df = pd.read_csv('../datasets/titanic/titanic_df.csv')
+
+# Применение one-hot-encoding для полового признака
+one_hot_enc = pd.get_dummies(df['Sex'], prefix='Sex')
+
+# Добавление новых признаков в исходный DataFrame
+df = pd.concat([df, one_hot_enc], axis=1)
+
+# Сохранение новой версии датасета
+df.to_csv('../datasets/titanic/titanic_df.csv', index=False)
