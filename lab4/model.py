af39ae5072e53d76781d827adc10d0999e8fbada
SergTa <tabakar456@mail.ru>
2024-06-06 14:49:25 +0300

    Edited path of saving datasets

--- lab4/model.py
+++ lab4/model.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 # Загрузка датасета
-df = pd.read_csv('../datasets/titanic_df.csv')
+df = pd.read_csv('./datasets/titanic_df.csv')
 
 # Применение one-hot-encoding для полового признака
 one_hot_enc = pd.get_dummies(df['Sex'], prefix='Sex')
@@ -12,4 +12,4 @@ one_hot_enc = pd.get_dummies(df['Sex'], prefix='Sex')
 df = pd.concat([df, one_hot_enc], axis=1)
 
 # Сохранение новой версии датасета
-df.to_csv('../datasets/titanic_df.csv', index=False)
+df.to_csv('./datasets/titanic_df.csv', index=False)
