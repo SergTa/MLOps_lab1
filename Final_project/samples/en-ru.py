c2082b78d7ee214c2500b9c85533db025846f92e
SergTa <tabakar456@mail.ru>
2024-06-24 15:52:27 +0300

    Edited samples

--- Final_project/samples/en-ru.py
+++ Final_project/samples/en-ru.py
@@ -1,6 +1,6 @@
 import requests
 
 url = "http://91.185.85.43:8000/translate/en-to-ru/"
-payload = {"text": "Never, never give up"}
+payload = {"text": "If you want to be OK, drinking vodka every day!"}
 response = requests.post(url, json=payload)
 print(response.json())
